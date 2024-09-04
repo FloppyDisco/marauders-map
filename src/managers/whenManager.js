55fb8817f03635c84f9706f6a44829bf4a840e61
FloppyDisco <joshuapaulpiasecki@gmail.com>
2024-09-04 16:28:35 +0200

    refactored WhenContext to use updated configs

--- src/managers/whenManager.js
+++ src/managers/whenManager.js
@@ -1,40 +1,42 @@
-const { SETTINGS } = require('../constants');
-
 const vscode = require('vscode');
+const {configKeys} = require('../managers/settingsManager');
+
 
 /**
- * Function to set the provided when clause context
- * @param {string} whenContext
- * @returns {null}
+ * Creates the necessary "when" clause context functions for the Page in VS Code.
+ *
+ * This function generates a formatted string for the when context based on the provided page title
+ * and returns an object containing the whenContext string, a function to set the whenContext, and
+ * a function to remove the whenContext.
+ *
+ * @param {string} mapPage - The title for this page of the map, used to generate the when context.
+ * @returns {{whenContext: string, setWhenContext: function, removeWhenContext: function}} - An object containing:
+ *   - `whenContext`: The formatted string to be used as the when context for this page.
+ *   - `setWhenContext`: A function to set the necessary when contexts in VS Code.
+ *   - `removeWhenContext`: A function to remove the necessary when contexts in VS Code.
  */
-exports.setPageWhenContext = (whenContext) => {
-    vscode.commands.executeCommand("setContext", SETTINGS.mapOpenContext, true);
-    vscode.commands.executeCommand("setContext", whenContext, true);
+function useWhenContext(mapPage) {
+    const whenContext = `${configKeys.maraudersMapPrefix}.${mapPage.replaceAll(" ", "_")}`;
+
+    /**
+     * Sets the page's "when" clause context.
+     */
+    function setWhenContext() {
+        vscode.commands.executeCommand("setContext",configKeys.mapOpenContext, true);
+        vscode.commands.executeCommand("setContext", whenContext, true);
+    }
+
+    /**
+     * Removes the page's "when" clause context.
+     */
+    function removeWhenContext() {
+        vscode.commands.executeCommand("setContext",configKeys.mapOpenContext, undefined);
+        vscode.commands.executeCommand("setContext", whenContext, undefined);
+    }
+
+    return { whenContext, setWhenContext, removeWhenContext };
 }
 
-/**
- * Function to remove the provided when clause context
- * @param {string} whenContext
- * @returns {null}
- */
-exports.removePageWhenContext = (whenContext) => {
-    vscode.commands.executeCommand("setContext", SETTINGS.mapOpenContext, undefined);
-    vscode.commands.executeCommand("setContext", whenContext, undefined);
+module.exports = {
+    useWhenContext
 }
-
-
-// /**
-//  * Function to set then map open when clause context
-//  * @returns {null}
-//  */
-// function setMapOpenContext(){
-//     vscode.commands.executeCommand("setContext", SETTINGS.mapOpenContext, true);
-// }
-
-// /**
-//  * Function to remove the mapOpen when clause context
-//  * @returns {null}
-//  */
-// function removeMapOpenContext(){
-//     vscode.commands.executeCommand("setContext", SETTINGS.mapOpenContext, undefined);
-// }
