480c89c7b09a052a5fdec2ec48069ef85036e839
FloppyDisco <joshuapaulpiasecki@gmail.com>
2024-09-04 13:31:09 +0200

    refactored settings manager

--- src/managers/settingsManager.js
+++ src/managers/settingsManager.js
@@ -1,12 +1,71 @@
 const vscode = require("vscode");
-const { SETTINGS, maraudersMapPrefix } = require("../constants");
 
-/**
- * Function to read the user's default mapDelay setting
- * @returns {number} the user defined value for map delay in seconds
- */
-exports.getDefaultMapDelay = () => {
-    return vscode.workspace
-        .getConfiguration(maraudersMapPrefix)
-        .get(SETTINGS.defaultMapDelay.key, SETTINGS.defaultMapDelay.value);
+
+const keys = {
+    maraudersMapPrefix: "maraudersMap",
+    mapOpenContext: "maraudersMapIsOpen",
+    defaultMapDelay: "defaultMapDelay",
+    pageIcon: "pageIcon",
+    subpageIcon: "subpageIcon",
+    spellIcon: "spellIcon",
+    displayMapTitle: "displayMapTitle",
+    displayCommandId: "displayCommandId",
+    commands: {
+        saveSpell: "expectoPatronum",
+        deleteSpell: "obliviate",
+        openMap: "iSolemnlySwearThatIAmUpToNoGood",
+        closeMap: "mischiefManaged",
+        displayMap: "lumos",
+        // revealSpell: "accio",
+        // erecto
+        // prior incantatoAccioAccio
+    },
+}
+
+const buttons = {
+    edit: {
+        id: "edit",
+        iconPath: new vscode.ThemeIcon("gear"),
+        tooltip: "Edit this Spell",
+    },
 };
+
+const mapIcon = "🗺️";
+
+
+// |---------------------------------|
+// |        Internal Settings        |
+// |---------------------------------|
+
+const internalConfigs = {
+    inputBoxTitle: `The Marauders Map ${mapIcon}`,
+    mapIcon: mapIcon, // might be able to get rid of this after refactor
+    buttons,
+    // pagesIcon: "$(files)",
+    // subpagesIcon: "$(arrow-right)",
+};
+
+
+
+let configCache;
+
+function updateStoredConfigs(){
+    configCache = new Map([
+        // contvert to key: value pairs for Map()
+        ...Object.entries(internalConfigs), // internal settings
+        ...Object.entries(vscode.workspace.getConfiguration(keys.maraudersMapPrefix)) // workspace settings
+    ])
+}
+
+exports.initialize = () => {
+    updateStoredConfigs();
+    vscode.workspace.onDidChangeConfiguration((event) => {
+        if (event.affectsConfiguration(keys.maraudersMapPrefix)){
+            updateStoredConfigs();
+        }
+    })
+}
+
+exports.useConfigs = () => {
+    return [ configCache, keys ]
+}
