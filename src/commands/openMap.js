77903b1680370f107a3746f90bd1e667bd9b1699
FloppyDisco <joshuapaulpiasecki@gmail.com>
2024-09-05 22:55:20 +0200

    refactored openMap

--- src/commands/openMap.js
+++ src/commands/openMap.js
@@ -1,14 +1,10 @@
 const vscode = require("vscode");
 // managers
-const KBmanager = require("../managers/keybindingsManager");
-const whenManager = require("../managers/whenManager");
 const settings = require("../managers/settingsManager");
-
-// components
-const mischiefStatusBar = require("../components/mischiefStatusBar");
-const solemnlySwearStatusBar = require("../components/solenmlySwearStatusBar");
-const pageStatusBar = require("../components/pageStatusBar");
-const maraudersMap = require("../components/maraudersMap");
+const whenMgr = require("../managers/whenManager");
+const mapManager = require("../managers/mapManager");
+const statusBarMgr = require("../managers/statusBarManager");
+const promptManager = require("../managers/promptManager");
 
 // |----------------------------------------------------------|
 // |        I Solemnly Swear That I Am Up To No Good ...      |
@@ -19,92 +15,45 @@ function register(context) {
         vscode.commands.registerCommand(
             settings.keys.commands.openMap,
             async ({ mapPage, mapDelay } = {}) => {
+
                 // remove any old UI elements from previous spells
-                // if (pagePrompt) {
-                //     pagePrompt.hide();
-                // }
+                mapManager.clean();
+                promptManager.clean();
+                statusBarMgr.clean();
 
-                const configs = settings.useConfigs();
-
-                mischiefStatusBar.removeIfExists();
-                solemnlySwearStatusBar.removeIfExists();
-
-                //   Create the Map!
-                // -------------------
-
-                maraudersMap.create();
-
-                // if no page do the no page stuff...
                 let selectedPageManually = false;
                 if (!mapPage) {
-                    //   Refactor Prompt Components
-                    // ------------------------------
 
-                    solemnlySwearStatusBar.create();
-                    mapPage = await promptUserForPage();
+                    // show generic status bar
+                    const solemnlySwearStatusBar =
+                        statusBarMgr.solenmlySwear.initialize();
+                    solemnlySwearStatusBar.show();
 
-                    // |---------------------|
-                    // |        *BUG*        |
-                    // |---------------------|
+                    mapPage = await promptManager.promptUserForPage()
 
-                    // clicking away
-                    // or escaping prompt
-                    // from Map does not get rid of the solemnlySwearStatusBar item
-
-                    solemnlySwearStatusBar.item.dispose();
+                    solemnlySwearStatusBar.dispose();
                     if (mapPage === undefined) {
                         return; // exit on 'Esc' key
                     }
                     selectedPageManually = true;
                 }
 
-                // if page do the page stuff...
-
                 //   Set the When Context
                 // ------------------------
-                const { whenContext, setWhenContext } = whenManager.initialize();
+                const { whenContext, setWhenContext, removeWhenContext } =
+                    whenMgr.initialize(mapPage);
+
                 setWhenContext();
-                pageStatusBar.create();
 
-                //   Update the Map
-                // ------------------
-                maraudersMap.updateTitle(mapPage);
-                maraudersMap.component.placeholder = "Choose your spell...";
+                statusBarMgr.page.initialize(mapPage).show();
 
-                const keybindings =
-                    KBmanager.getKeybindingsForPage(whenContext);
-
-                const spells =
-                    maraudersMap.convertKeybindingsToQuickPickItems(
-                        keybindings
-                    );
-
-                maraudersMap.addItems([
-                    ...spells,
-                    {
-                        label: "$(add) New Spell",
-                        command: settings.keys.commands.saveSpell,
-                        args: { mapPage },
-                        alwaysShow: true,
-                    },
-                ]);
-
-                const mapDelayTime =
-                    mapDelay !== undefined
-                        ? mapDelay
-                        : configs.get(settings.keys.defaultMapDelay);
-
-                if (!selectedPageManually && mapDelayTime) {
-                    setTimeout(() => {
-                        // show map after delay
-                        if (maraudersMap.component && !maraudersMap.visible) {
-                            maraudersMap.showComponent();
-                        }
-                    }, mapDelayTime);
-                } else {
-                    // map delay is zero, show map!
-                    maraudersMap.showComponent();
-                }
+                mapManager.initialize({
+                    mapDelay,
+                    mapPage,
+                    selectedPageManually,
+                    whenContext,
+                    removeWhenContext
+                });
             }
         )
     );
