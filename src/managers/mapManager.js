530a19f41a7235f119f50e75bb631c83feb742b3
FloppyDisco <joshuapaulpiasecki@gmail.com>
2024-09-07 19:42:31 +0200

    refactored initialize, created callMap and SelectOrder funtions

--- src/managers/mapManager.js
+++ src/managers/mapManager.js
@@ -13,113 +13,130 @@ let mapOpenTimer;
  * Function to create and return the maraudersMap
  * @returns {vscode.QuickPick} The maraudersMap quick pick
  */
-function initialize({ mapDelay, mapPage, selectedPageManually, whenContext, removeWhenContext }) {
+function initialize({ mapPage, whenContext, removeWhenContext }) {
+  if (maraudersMap) {
+    // exists
+    maraudersMap.dispose();
+  }
 
-    if (maraudersMap) {
-        // exists
-        maraudersMap.dispose();
-    }
+  const configs = settings.useConfigs();
 
-    const configs = settings.useConfigs();
+  //   Create Map
+  // --------------
 
-    //   Create Map
-    // --------------
+  maraudersMap = vscode.window.createQuickPick();
 
-    maraudersMap = vscode.window.createQuickPick();
+  const displayTitle = configs.get(settings.keys.displayMapTitle);
+  if (displayTitle) {
+    maraudersMap.title = `${configs.get(settings.keys.titleIcon)} ${mapPage}`;
+  }
+  maraudersMap.placeholder = "Choose your spell...";
+  maraudersMap.onDidTriggerItemButton(promptsMgr.menuItemButtonTrigger);
 
-    const displayTitle = configs.get(settings.keys.displayMapTitle);
-    if (displayTitle) {
-        maraudersMap.title = `${configs.get(
-            settings.keys.titleIcon
-        )} ${mapPage}`;
-    }
-    maraudersMap.placeholder = "Choose your spell...";
-    maraudersMap.onDidTriggerItemButton(promptsMgr.menuItemButtonTrigger);
+  // |-----------------------|
+  // |        Feature        |
+  // |-----------------------|
+  // go back page button for nested pages?
+  // maraudersMap.buttons= [];
 
-    // |-----------------------|
-    // |        Feature        |
-    // |-----------------------|
-    // go back page button for nested pages?
-    // maraudersMap.buttons= [];
+  maraudersMap.onDidHide(() => {
+    removeWhenContext();
+    maraudersMap.dispose();
+    statusBarMgr.page.use().dispose();
+  });
 
+  maraudersMap.onDidChangeSelection(([selection]) => {
+    //   Magic Sauce Is HERE
+    // -----------------------
+    removeWhenContext();
+    maraudersMap.dispose();
+    statusBarMgr.page.use().dispose();
+    vscode.commands.executeCommand(
+      // well ok, it's here...
+      selection.command,
+      selection.args
+    );
+  });
 
+  //   Get Keybindings
+  // -------------------
 
-    maraudersMap.onDidHide(() => {
-        removeWhenContext();
-        maraudersMap.dispose();
-        statusBarMgr.page.use().dispose();
-    });
+  const keybindings = keybindingsMgr.getKeybindingsForPage(whenContext);
 
-    maraudersMap.onDidChangeSelection(([selection]) => {
-        //   Magic Sauce Is HERE
-        // -----------------------
-        removeWhenContext();
-        maraudersMap.dispose();
-        statusBarMgr.page.use().dispose();
-        vscode.commands.executeCommand( // well ok, it's here...
-            selection.command,
-            selection.args
-        );
-    });
+  const spells = keybindingsMgr.convertToItems(keybindings);
 
+  maraudersMap.items = [
+    ...spells,
+    {
+      label: "$(add) New Spell",
+      command: settings.keys.commands.saveSpell,
+      args: { mapPage },
+      alwaysShow: true,
+    },
+  ];
+}
 
-    //   Get Keybindings
-    // -------------------
+function callMap({ mapDelay, selectedPageManually }) {
+  const configs = settings.useConfigs();
 
-    const keybindings = keybindingsMgr.getKeybindingsForPage(whenContext);
+  const mapDelayTime =
+    mapDelay !== undefined
+      ? mapDelay
+      : configs.get(settings.keys.defaultMapDelay);
 
-    const spells = keybindingsMgr.convertToItems(keybindings);
-
-    maraudersMap.items = [
-        ...spells,
-        {
-            kind: vscode.QuickPickItemKind.Separator
-        },
-        {
-            label: "$(add) New Spell",
-            command: settings.keys.commands.saveSpell,
-            args: { mapPage },
-            alwaysShow: true,
-        },
-    ]
-
-    const mapDelayTime =
-        mapDelay !== undefined
-            ? mapDelay
-            : configs.get(settings.keys.defaultMapDelay);
-
-    if (!selectedPageManually && mapDelayTime) {
-        mapOpenTimer = setTimeout(() => {
-            // show map after delay
-            if (maraudersMap && !maraudersMap._visible) {
-                maraudersMap.show();
-            }
-        }, mapDelayTime);
-    } else {
-        // map delay is zero, show map!
+  if (!selectedPageManually && mapDelayTime) {
+    mapOpenTimer = setTimeout(() => {
+      // show map after delay
+      if (maraudersMap && !maraudersMap._visible) {
         maraudersMap.show();
-    }
+      }
+    }, mapDelayTime);
+  } else {
+    // map delay is zero, show map!
+    maraudersMap.show();
+  }
 }
 
 function use() {
-    return maraudersMap;
+  return maraudersMap;
+}
+
+async function selectOrder() {
+  maraudersMap.placeholder = "What Spell shall the separator go above?";
+  return new Promise((resolve) => {
+    maraudersMap.onDidChangeSelection(([selection]) => {
+      if (selection) {
+        resolve(
+          maraudersMap.items.findIndex((item) => item.label === selection.label)
+        );
+      } else {
+        resolve(undefined);
+      }
+      maraudersMap.dispose();
+    });
+    maraudersMap.onDidHide(() => {
+      maraudersMap.dispose();
+      resolve(undefined);
+    });
+    maraudersMap.show();
+  });
 }
 
 function clean() {
-    if(maraudersMap){
-        maraudersMap.dispose();
-    }
+  if (maraudersMap) {
+    maraudersMap.dispose();
+  }
 }
 
-function cancelTimer(){
-    console.log('cancelling timer:',mapOpenTimer);
-
-    clearTimeout(mapOpenTimer);
+function cancelTimer() {
+  clearTimeout(mapOpenTimer);
 }
 
 module.exports = {
-    initialize,
-    use,
-    clean,
-    cancelTimer,
+  initialize,
+  callMap,
+  use,
+  selectOrder,
+  clean,
+  cancelTimer,
 };
